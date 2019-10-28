e45425ee70cbce4970819a55d410c34da79c2e7f
Trevor Bakker <trevor.bakker@uta.edu>
2019-10-28 22:38:04 +0000

    Add buffer blocks

--- tests/bfwf.c
+++ tests/bfwf.c
@@ -5,6 +5,7 @@
 int main()
 {
   char * ptr1 = ( char * ) malloc ( 65535 );
+  char * ptr4 = ( char * ) malloc ( 65 );
   char * ptr2 = ( char * ) malloc ( 6000 );
 
   printf("Worst fit should pick this one: %p\n", ptr1 );
@@ -12,6 +13,8 @@ int main()
 
   free( ptr1 ); 
   free( ptr2 ); 
+
+  ptr4 = ptr4;
 
   char * ptr3 = ( char * ) malloc ( 1000 );
   printf("Chosen address: %p\n", ptr3 );
--- tests/ffnf.c
+++ tests/ffnf.c
@@ -5,7 +5,13 @@
 int main()
 {
   char * ptr1 = ( char * ) malloc ( 1000 );
+
+  char * ptr6= ( char * ) malloc ( 10 );
+  
   char * ptr2 = ( char * ) malloc ( 6000 );
+
+  char * ptr7= ( char * ) malloc ( 10 );
+
   char * ptr3 = ( char * ) malloc ( 1000 );
 
   printf("First fit should pick this one: %p\n", ptr1 );
@@ -18,6 +24,8 @@ int main()
   char * ptr5 = ( char * ) malloc ( 6000 );
 
   ptr5 = ptr5;
+  ptr7 = ptr7;
+  ptr6 = ptr6;
 
   char * ptr4 = ( char * ) malloc ( 1000 );
   printf("Chosen address: %p\n", ptr4 );
