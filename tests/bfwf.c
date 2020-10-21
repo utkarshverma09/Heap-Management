7b33caa752187f3e700d9d36bfe69aefdb9d61eb
CSE 3320 Developer <devuser@cse3320>
2020-10-20 21:01:33 -0400

    Added padding buffers to keep test cases working after coalesce

--- tests/bfwf.c
+++ tests/bfwf.c
@@ -5,7 +5,9 @@
 int main()
 {
   char * ptr1 = ( char * ) malloc ( 65535 );
+  char * buffer1 = ( char * ) malloc( 1 );
   char * ptr4 = ( char * ) malloc ( 65 );
+  char * buffer2 = ( char * ) malloc( 1 );
   char * ptr2 = ( char * ) malloc ( 6000 );
 
   printf("Worst fit should pick this one: %p\n", ptr1 );
@@ -14,6 +16,8 @@ int main()
   free( ptr1 ); 
   free( ptr2 ); 
 
+  buffer1 = buffer1;
+  buffer2 = buffer2;
   ptr4 = ptr4;
 
   char * ptr3 = ( char * ) malloc ( 1000 );
--- tests/ffnf.c
+++ tests/ffnf.c
@@ -5,13 +5,13 @@
 int main()
 {
   char * ptr1 = ( char * ) malloc ( 1000 );
-
+  char * buf1 = ( char * ) malloc ( 1 );
   char * ptr6= ( char * ) malloc ( 10 );
-  
+  char * buf2 = ( char * ) malloc ( 1 );
   char * ptr2 = ( char * ) malloc ( 6000 );
-
+  char * buf3 = ( char * ) malloc ( 1 );
   char * ptr7= ( char * ) malloc ( 10 );
-
+  char * buf4 = ( char * ) malloc ( 1 );
   char * ptr3 = ( char * ) malloc ( 1000 );
 
   printf("First fit should pick this one: %p\n", ptr1 );
@@ -26,6 +26,10 @@ int main()
   ptr5 = ptr5;
   ptr7 = ptr7;
   ptr6 = ptr6;
+  buf1 = buf1;
+  buf2 = buf2;
+  buf3 = buf3;
+  buf4 = buf4;
 
   char * ptr4 = ( char * ) malloc ( 1000 );
   printf("Chosen address: %p\n", ptr4 );
